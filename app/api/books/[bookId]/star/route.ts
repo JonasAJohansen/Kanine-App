4dd75a4b4f9eeb19a5de680f269cbdd5d9dcb16d
Jonas Johansen <110419848+JonasAJohansen@users.noreply.github.com>
2024-09-03 18:01:35 +0200

    Update route.ts

--- app/api/books/[bookId]/star/route.ts
+++ app/api/books/[bookId]/star/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { getAuth } from '@clerk/nextjs/server';
-import prisma from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 
 export async function POST(
   req: Request,
